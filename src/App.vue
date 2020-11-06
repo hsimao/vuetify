d7893c45a23e5181d3535c736a39cc6f143471a7
hsimao <e087754958@gmail.com>
2020-11-06 21:17:43 +0800

    add: vue-router, login views component

--- src/App.vue
+++ src/App.vue
@@ -3,50 +3,32 @@
     <v-app-bar app color="primary" dark>
       <v-toolbar-title>Vuetify Dashboard</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn text rounded>Home</v-btn>
-      <v-btn text rounded>Login</v-btn>
+      <v-btn
+        v-for="link in links"
+        :key="`${link.label}-header-link`"
+        text
+        rounded
+        :to="link.url"
+      >
+        {{ link.label }}
+      </v-btn>
     </v-app-bar>
     <v-main>
-      <!-- Login Module -->
-      <v-card width="400" class="mx-auto mt-5 pa-5">
-        <v-card-title>
-          <h1 class="display-1">Login</h1>
-        </v-card-title>
-        <v-card-text>
-          <v-form>
-            <v-text-field label="Username" prepend-icon="mdi-account-circle" />
-            <v-text-field
-              :type="showPassword ? 'text' : 'password'"
-              label="Password"
-              prepend-icon="mdi-lock"
-              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-              @click:append="showPassword = !showPassword"
-              @click:prepend="handleClickPrepend"
-              @click="handleClick"
-            />
-          </v-form>
-        </v-card-text>
-        <v-divider></v-divider>
-        <v-card-actions>
-          <v-btn color="success">Register</v-btn>
-          <!-- spacer 墊片 佔位空間 -->
-          <v-spacer></v-spacer>
-          <v-btn color="info">Login</v-btn>
-        </v-card-actions>
-      </v-card>
+      <router-view></router-view>
     </v-main>
 
     <v-footer color="primary lighten-1" padless>
       <v-row justify="center" no-gutters>
         <v-btn
           v-for="link in links"
-          :key="link"
+          :key="`${link.label}-footer-link`"
           color="white"
           text
           rounded
           class="my-2"
+          :to="link.url"
         >
-          {{ link }}
+          {{ link.label }}
         </v-btn>
         <v-col class="primary lighten-2 py-4 text-center white--text" cols="12">
           {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
@@ -61,16 +43,16 @@ export default {
   name: "App",
   components: {},
   data: () => ({
-    showPassword: false,
-    links: ["Home", "Login"]
-  }),
-  methods: {
-    handleClickPrepend() {
-      console.log("handleClickPrepend");
-    },
-    handleClick() {
-      console.log("handleClick");
-    }
-  }
+    links: [
+      {
+        label: "Home",
+        url: "/"
+      },
+      {
+        label: "Login",
+        url: "/login"
+      }
+    ]
+  })
 };
 </script>
---
+++ src/views/Login.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-card width="400" class="mx-auto mt-5 pa-5">
+    <v-card-title>
+      <h1 class="display-1">Login</h1>
+    </v-card-title>
+    <v-card-text>
+      <v-form>
+        <v-text-field label="Username" prepend-icon="mdi-account-circle" />
+        <v-text-field
+          :type="showPassword ? 'text' : 'password'"
+          label="Password"
+          prepend-icon="mdi-lock"
+          :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+          @click:append="showPassword = !showPassword"
+          @click:prepend="handleClickPrepend"
+          @click="handleClick"
+        />
+      </v-form>
+    </v-card-text>
+    <v-divider></v-divider>
+    <v-card-actions>
+      <v-btn color="success">Register</v-btn>
+      <!-- spacer 墊片 佔位空間 -->
+      <v-spacer></v-spacer>
+      <v-btn color="info">Login</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data: () => ({
+    showPassword: false
+  }),
+  methods: {
+    handleClickPrepend() {
+      console.log("handleClickPrepend");
+    },
+    handleClick() {
+      console.log("handleClick");
+    }
+  }
+};
+</script>
+
+<style scoped></style>
